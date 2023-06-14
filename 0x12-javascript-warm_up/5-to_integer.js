4a737ccc75a9a3a7adad3a873b16016906116118
David Agbaniyaka <fiyin.agba@gmail.com>
2023-06-14 10:10:10 -0700

    Update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,3 @@
 #!/usr/bin/node
-console.log(`My number: ${parseInt(process.argv[2])}`)
+if (parseInt(process.argv[2] !== number)) console.log('Not a number');
+else console.log(`My number: ${parseInt(process.argv[2])}`)
