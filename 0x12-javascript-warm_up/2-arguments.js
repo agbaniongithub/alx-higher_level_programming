cea49f6c724492c56cabc131f804b7b6295b5dcf
David Agbaniyaka <fiyin.agba@gmail.com>
2023-06-14 03:53:54 -0700

    Update

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function arguments() {
+  if (argv[2] = false) {
+   console.log('No Argument')
+  }
+  else if (argv[2] = true && argv[3] = false) {
+    console.log('Argument found')
+  }
+  console.log('Arguments found')
+
+}
